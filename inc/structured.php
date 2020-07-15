f77db2bb3e24ed02d8d6ac8e727330f07c1a8f11
Oh Morita <65946160+mrtc-jp@users.noreply.github.com>
2020-07-15 15:49:17 +0900

    1.2.2

--- inc/structured.php
+++ inc/structured.php
@@ -6,8 +6,6 @@
 "description":"岐阜県岐阜市を拠点とするデザイン事務所。Webデザインをメインに、企業や店舗のマーケティングをサポートします。",
 "url": "https://mrtc.jp/",
 "logo": "https://mrtc.jp/wp-content/themes/mrtc/img/logo.png",
-"telephone": "058-374-0766",
-"address": "〒500-8307 岐阜市弁天町1-20-2",
 "image": "https://mrtc.jp/wp-content/themes/mrtc/screenshot.png"
 }
 </script>
